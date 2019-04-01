0b91342fbc80277a9c8c2e44f7c619cba5c7afb8
Lars Jansøn Engvik <lars@engvik.nu>
2019-04-01 09:00:42 +0200

    feat(binary): add golang

--- install.sh
+++ install.sh
@@ -26,3 +26,6 @@ sudo snap install jq evince
 curl -L -o stern https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
 checksum stern a0335b298f6a7922c35804bffb32a68508077b2f35aaef44d9eb116f36bc7eda
 installBinary stern
+curl -L -o golang.tar.gz https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf golang.tar.gz
+rm -rf golang.tar.gz
