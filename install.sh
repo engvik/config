bd0756e235c60272bdba1dee43ab5f74f7faf72f
Lars Jansøn Engvik <lars@engvik.nu>
2019-04-23 15:01:46 +0200

    chore(go): 1.12.4

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ checksum stern a0335b298f6a7922c35804bffb32a68508077b2f35aaef44d9eb116f36bc7eda
 installBinary stern
 
 # Go
-curl -L -o golang.tar.gz https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
+curl -L -o golang.tar.gz https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf golang.tar.gz
 rm -rf golang.tar.gz
 
