a0414449abbec26c722fd92f8c7402da32a7e5a3
Lars Jansøn Engvik <lars@engvik.nu>
2019-04-28 10:51:26 +0200

    feat(vim): install vim separatly

---
+++ install-vim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get neovim
+sudo apt install neovim
+
+# Install autoload
+./vim-autoload.sh
---
+++ vim-autoload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
