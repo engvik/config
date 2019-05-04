7073a7c538d6885ce2168b3ddace2664a5164433
Lars Jansøn Engvik <lars@engvik.nu>
2019-05-04 18:20:30 +0200

    feat(install): add more dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# VIM
+# vim
 mkdir -p ~/.config/nvim
 cp dotfiles/init.vim ~/.config/nvim/init.vim
+
+# bash
+cp dotfiles/.bashrc ~/
+cp dotfiles/.bash_aliases ~/
+
+# git
+cp dotfiles/.gitconfig ~/
+
