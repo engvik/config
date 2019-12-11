61b17012ad16f4dc7c1554ead54cb10e9da2abb6
Lars Jansøn Engvik <lars@engvik.nu>
2019-12-11 15:21:13 +0100

    feat(sh): add golangci-lint

--- install-sh.sh
+++ install-sh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.21.0
