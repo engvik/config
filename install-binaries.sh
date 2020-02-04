800766b0bdcc79c690365094bc801e355f1313dc
Lars Jansøn Engvik <lars@engvik.nu>
2020-02-04 08:20:06 +0100

    refactor(nvim): get nvim as binary, not install from apt

--- install-binaries.sh
+++ install-binaries.sh
@@ -16,17 +16,24 @@ function installBinary {
     sudo ln -s /usr/lib/$1 /usr/bin/$1
 }
 
-
-# Stern
-curl -L -o stern https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
-checksum stern a0335b298f6a7922c35804bffb32a68508077b2f35aaef44d9eb116f36bc7eda
-installBinary stern
+# nvim
+curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+tar zxf nvim.tar.gz
+sudo mv nvim-linux64/share/* /usr/local/share
+sudo mv nvim-linux64/bin/* /usr/local/bin
+rm -rf nvim-linux64/
+rm nvim.tar.gz
 
 # Go
 curl -L -o golang.tar.gz https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
 checksum golang.tar.gz 512103d7ad296467814a6e3f635631bd35574cab3369a97a323c9a585ccaa569
 sudo tar -C /usr/local -xzf golang.tar.gz
-rm -rf golang.tar.gz
+rm golang.tar.gz
+
+# Stern
+curl -L -o stern https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
+checksum stern a0335b298f6a7922c35804bffb32a68508077b2f35aaef44d9eb116f36bc7eda
+installBinary stern
 
 # Protoc
 curl -L -o protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
--- install-vim.sh
+++ install-vim.sh
@@ -3,7 +3,12 @@
 # Get neovim
 
 if ! [ -x "$(command -v nvim)" ]; then
-    sudo apt install neovim
+    curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+    tar zxf nvim.tar.gz
+    sudo mv nvim-linux64/share/* /usr/local/share
+    sudo mv nvim-linux64/bin/* /usr/local/bin
+    rm -rf nvim-linux64/
+    rm nvim.tar.gz
 fi
 
 # Install vim-plug
