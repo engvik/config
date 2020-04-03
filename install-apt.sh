4166912af4a9f6cf3e8c67c22a9f9cc46b40b8cb
Lars Jansøn Engvik <lars@engvik.nu>
2020-04-03 10:23:39 +0200

    feature(apt): add curl

--- install-apt.sh
+++ install-apt.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install evince unzip ntp fzf sshuttle postgresql docker.io ddgr python3 python3-pip
+sudo apt install curl evince unzip ntp fzf sshuttle postgresql docker.io ddgr python3 python3-pip
 
