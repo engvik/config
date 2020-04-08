437f70da2890d4e9cbc1828284244949e4ef8a73
Lars Jansøn Engvik <lars@engvik.nu>
2020-04-08 08:19:58 +0200

    feature(apt): silversearcher-ag

--- install-apt.sh
+++ install-apt.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install curl evince unzip ntp fzf sshuttle postgresql docker.io ddgr python3 python3-pip htop
+sudo apt install curl evince unzip ntp fzf sshuttle postgresql docker.io ddgr python3 python3-pip htop silversearcher-ag
 
